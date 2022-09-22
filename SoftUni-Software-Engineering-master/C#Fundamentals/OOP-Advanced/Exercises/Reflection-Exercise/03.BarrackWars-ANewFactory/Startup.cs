78076dc2714fe2de991b2e10d6040981a153f05a
BAAL98 <baal98@gmail.com>
2022-09-22 06:14:13 +0300

    Exercise

---
+++ SoftUni-Software-Engineering-master/C#Fundamentals/OOP-Advanced/Exercises/Reflection-Exercise/03.BarrackWars-ANewFactory/Startup.cs
@@ -0,0 +1,11 @@
+﻿public class Startup
+{
+    public static void Main()
+    {
+        IRepository repository = new UnitRepository();
+        IUnitFactory unitFactory = new UnitFactory();
+        IRunnable engine = new Engine(repository, unitFactory);
+        engine.Run();
+    }
+}
+

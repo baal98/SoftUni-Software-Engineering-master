23ae94d75156900c90c2743c79bbc3a58d305dba
BAAL98 <baal98@gmail.com>
2022-09-22 06:13:41 +0300

    Exercise

---
+++ SoftUni-Software-Engineering-master/C#Fundamentals/OOP-Advanced/Exercises/Reflection-Exercise/02.BlackBoxInteger/Startup.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+
+public class Startup
+{
+    public static void Main()
+    {
+        var classType = typeof(BlackBoxInt);
+        var methodsInfo = classType.GetMethods(BindingFlags.NonPublic | BindingFlags.Instance);
+        var instance = (BlackBoxInt)Activator.CreateInstance(classType, true);
+
+        var input = Console.ReadLine();
+        while (!input.Equals("END"))
+        {
+            var lineArgs = input.Split('_');
+            var currentMethod = methodsInfo.FirstOrDefault(x => x.Name.Equals(lineArgs[0]));
+            currentMethod.Invoke(instance, new object[] { int.Parse(lineArgs[1]) });
+
+            var innerValue = classType.GetFields(BindingFlags.NonPublic | BindingFlags.Instance).FirstOrDefault(x => x.Name.Equals("innerValue")).GetValue(instance);
+
+            Console.WriteLine(innerValue);
+            input = Console.ReadLine();
+        }
+    }
+}

609061acf7825b7ac63e45790d66b9a2c3bcc33f
BAAL98 <baal98@gmail.com>
2022-11-02 12:15:50 +0200

    Exercise

---
+++ SoftUni-Software-Engineering-master/JS_ADVANCED/Arrays and Nested Arrays/print anArrayWithAGivenDelimiter.js
@@ -0,0 +1,14 @@
+function PrintAnArrayWithAGivenDelimiter(arr, delimiter) {
+    let outputArr = arr;
+
+    console.log(outputArr.join(delimiter));
+}
+
+PrintAnArrayWithAGivenDelimiter(['How about no?', 
+'I',
+'will', 
+'not', 
+'do', 
+'it!'], 
+'_'
+);
